40ebf745b0c5ada6e3303afcb13da01915cdc470
Ellen <lidenellen@gmail.com>
2025-07-09 13:33:33 +0200

    Lagt till en footer-komponent med kontaktinformation och länkar. Responsiv styling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
