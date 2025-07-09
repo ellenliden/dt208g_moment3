da91309acf29e635dcba1ca1cb6badfc80c7d552
Ellen <lidenellen@gmail.com>
2025-07-09 12:53:53 +0200

    Flyttat navbar till egen komponent och lagt till  responsiv styling och funktionalitet. Skapade hamburger-meny för mobil/tablet navigering. La till en sida (som kompnent) portfolio.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Portfolio } from './portfolio/portfolio';
 import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
+  { path: 'portfolio', component: Portfolio },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'not-found', component: NotFound },
   { path: '**', component: NotFound },
