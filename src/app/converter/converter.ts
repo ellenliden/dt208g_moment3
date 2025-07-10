551a61f15e81aba0ac735318ad0aa59708478859
Ellen <lidenellen@gmail.com>
2025-07-10 13:11:39 +0200

    Skapat en ‘enhets-konverterare’ med meter/fot och celsius/farenheit med html och typescript (innehåll och funktion). Använder tvåvägsdatabindning med ngModel för realtidskonvertering, uppdaterat routing-konfiguration för att inkludera converter-sidan samt lagt till länk till sidan i navbar.

--- src/app/converter/converter.ts
+++ src/app/converter/converter.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './converter.html',
-  styleUrl: './converter.css'
+  styleUrl: './converter.css',
 })
 export class Converter {
+  // Meter/Feet
+  meter: number | null = null;
+  feet: number | null = null;
 
+  // Celsius/Fahrenheit
+  celsius: number | null = null;
+  fahrenheit: number | null = null;
+
+  meterToFeet() {
+    this.feet = this.meter !== null ? +(this.meter * 3.28084).toFixed(2) : null;
+  }
+
+  feetToMeter() {
+    this.meter = this.feet !== null ? +(this.feet / 3.28084).toFixed(2) : null;
+  }
+
+  celsiusToFahrenheit() {
+    this.fahrenheit =
+      this.celsius !== null ? +((this.celsius * 9) / 5 + 32).toFixed(2) : null;
+  }
+
+  fahrenheitToCelsius() {
+    this.celsius =
+      this.fahrenheit !== null
+        ? +(((this.fahrenheit - 32) * 5) / 9).toFixed(2)
+        : null;
+  }
 }
